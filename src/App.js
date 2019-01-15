06927566b1b0b13850ce7c22368a985c375de680
Dominic Brandel <dominic.brandel@innoplexia.com>
2019-01-15 12:03:45 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import AddStudent from './AddStudent';
+import StudentOverview from './StudentOverview';
 import './App.css';
+import './index.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isAuthenticated: true,
+      user: {
+        username: '',
+        password: ''
+      },
+    }
+  }
+
+  setAuthentification = (isAuthenticated) => {
+    console.log('Hallo aus App')
+    console.log(this.state)
+    this.setState({
+      isAuthenticated: isAuthenticated
+    })
+  }
+
   render() {
+
     return (
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <a className='btn btn-warning btn-large linkmargin' href='#/add-student' target='_self'>Schüler hinzufügen</a>
+        <a className='btn btn-warning btn-large linkmargin' href='#/student-overview' target='_self'>Alle Schüler</a>
+        <a className='btn btn-info btn-large linkmargin' href='http://localhost:8080/schule/export' target='_self'>Download CSV-Export</a>
+
+          <Router>
+            <Switch>
+              <Route exact path="/add-student" render={(props)=> <AddStudent {...props} setAuthentification={this.setAuthentification} isAuthenticated={this.state.isAuthenticated} />} />
+              <Route exact path="/student-overview" render={()=> <StudentOverview setAuthentification={this.setAuthentification} isAuthenticated={this.state.isAuthenticated} />} />
+
+              <Redirect to="/student-overview" render={()=> <StudentOverview setAuthentification={this.setAuthentification} isAuthenticated={this.state.isAuthenticated} />} />
+            </Switch>
+          </Router>
       </div>
     );
   }
